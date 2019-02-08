9762994b65afcfbdd7ff8d674b173de70a9075b3
Ryan Knauber <ryanknauber@yahoo.com>
2019-02-07 18:00:46 -0800

    Slight modifications.

--- explore.py
+++ explore.py
@@ -10,8 +10,13 @@ mavs = [team for team in nba_teams
 
 from nba_api.stats.endpoints import teamyearbyyearstats
 mavs_yby_stats = teamyearbyyearstats.TeamYearByYearStats(team_id=mavs['id']).get_data_frames()[0]
+lockout_year_index = mavs_yby_stats[mavs_yby_stats['YEAR'] == '2011-12'].index.values[0]
 reg_season_df = pd.DataFrame(columns=mavs_yby_stats)
 
 for team in nba_teams :
-    lockout_year_index = mavs_yby_stats[mavs_yby_stats['YEAR'] == '2011-12'].index.values[0]
-    mavs_yby_stats = mavs_yby_stats[(mavs_yby_stats.index > lockout_year_index) & (mavs_yby_stats['GP'] > 69)]
+    team_yby_stats = teamyearbyyearstats.TeamYearByYearStats(team_id=team['id']).get_data_frames()[0]
+    team_yby_stats = team_yby_stats[(team_yby_stats.index > lockout_year_index) & (team_yby_stats['GP'] > 69)]
+    reg_season_df.append(team_yby_stats)
+
+print(reg_season_df.head())
+print(reg_season_df.describe())
