dc53d18ec052ef01a8b9b26620ff46f725d5c7f5
Ryan Knauber <ryanknauber@yahoo.com>
2019-02-07 17:55:33 -0800

    Began building table to nba team reg season records and stats.

--- explore.py
+++ explore.py
@@ -1,5 +1,17 @@
-from nba_api.stats.endpoints import playercareerstats
+import numpy as np
+import pandas as pd
 
-# Anthony Davis
-career = playercareerstats.PlayerCareerStats(player_id='203076')
-career.get_data_frames()[0]
+from nba_api.stats.static import teams
+# get_teams returns a list of 30 dictionaries, each an NBA team.
+nba_teams = teams.get_teams()
+print('Number of teams fetched: {}'.format(len(nba_teams)))
+mavs = [team for team in nba_teams
+         if team['full_name'] == 'Dallas Mavericks'][0]
+
+from nba_api.stats.endpoints import teamyearbyyearstats
+mavs_yby_stats = teamyearbyyearstats.TeamYearByYearStats(team_id=mavs['id']).get_data_frames()[0]
+reg_season_df = pd.DataFrame(columns=mavs_yby_stats)
+
+for team in nba_teams :
+    lockout_year_index = mavs_yby_stats[mavs_yby_stats['YEAR'] == '2011-12'].index.values[0]
+    mavs_yby_stats = mavs_yby_stats[(mavs_yby_stats.index > lockout_year_index) & (mavs_yby_stats['GP'] > 69)]
