e4ca94b2a1de1478465492f97443ab641f4b2caa
Ryan Knauber <ryan@viztric.com>
2019-05-24 10:54:41 -0700

    Refreshing repo.

---
+++ build_plan.md
@@ -0,0 +1,22 @@
+# Building an NBA model
+
+## Basic Ideas, in order of complexity
+
+### Easier
+- Predict who will win the NBA Finals in a season, based on reg season team stats
+- Predict which highest round NBA playoffs each team will achieve in a season, based on team stats
+
+### Harder
+- Find the most underused/underpaid players in the NBA
+- Predict who will win an NBA game based on player level stats
+
+### Really hard
+- Set up NBA game simulations to predict game winner likelihood
+
+
+## Current Plan
+#### Start with "Predict who will win the NBA Finals in a season, based on reg season team stats"
+- Step 1: Data pull and munging of reg season team stats, in organized code structure.
+- Step 2: Visualization and EDA.
+- Step 3: Feature engineering, reduction, and scaling.
+- Step 4: Grid search.
--- pull_reg_season_data.py
+++ pull_reg_season_data.py
@@ -5,27 +5,27 @@ from nba_api.stats.static import teams
 # get_teams returns a list of 30 dictionaries, each an NBA team.
 nba_teams = teams.get_teams()
 print('Number of teams fetched: {}'.format(len(nba_teams)))
-mavs = [team for team in nba_teams
+mavs_id = [team for team in nba_teams
          if team['full_name'] == 'Dallas Mavericks'][0]
 
+# Get Mavs reg season yby DF to create empty DF with appropriate columns
 from nba_api.stats.endpoints import teamyearbyyearstats
-mavs_yby_stats = teamyearbyyearstats.TeamYearByYearStats(team_id=mavs['id']).get_data_frames()[0]
+reg_season_yby_mavs = teamyearbyyearstats.TeamYearByYearStats(team_id=mavs_id['id']).get_data_frames()[0]
+reg_season_yby = pd.DataFrame(columns=reg_season_yby_mavs.columns)
 
-reg_season_df = pd.DataFrame(columns=mavs_yby_stats.columns)
+print(reg_season_yby)
 
-print(reg_season_df)
-
+# Get Reg Season YBY stats for all teams, beginning in 2011-12
 for team in nba_teams :
     team_yby_stats = teamyearbyyearstats.TeamYearByYearStats(team_id=team['id']).get_data_frames()[0]
     lockout_year_index = team_yby_stats[team_yby_stats['YEAR'] == '2011-12'].index.values[0]
     team_yby_stats = team_yby_stats[(team_yby_stats.index > lockout_year_index) & (team_yby_stats['GP'] > 69)]
-    reg_season_df = reg_season_df.append(team_yby_stats)
+    reg_season_yby = reg_season_yby.append(team_yby_stats)
 
-print(reg_season_df.head())
-print(reg_season_df.describe())
-print(reg_season_df.shape)
-reg_season_df['TEAM_CITY'].nunique()
-reg_season_df = reg_season_df.set_index(['TEAM_ID', 'YEAR'])
-reg_season_df.to_csv('D:\\Python\\zstorage\\nba\\reg_season.csv')
-
+print(reg_season_yby.head())
+print(reg_season_yby.describe())
+print(reg_season_yby.shape)
+reg_season_yby['TEAM_CITY'].nunique()
+reg_season_yby = reg_season_yby.set_index(['TEAM_ID', 'YEAR'])
+reg_season_yby.to_csv('D:\\Python\\zstorage\\nba\\reg_season_yby.csv')
 
